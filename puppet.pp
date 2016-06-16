929d8a7be39d4e54d5db7e1bbd658fd94fb48f78
baluevD <newarmlet@gmail.com>
2016-06-16 20:44:25 +0300

    Update puppet.pp

--- puppet.pp
+++ puppet.pp
@@ -1,8 +1,13 @@
 node default {
-  class {'oracle_java':
-    version => '8u45',
-    type => 'jdk'
-  }
+  #class {'oracle_java':
+  #  version => '8u45',
+  #  type => 'jdk'
+  #}
+  java::oracle { 'jdk8' :
+  ensure  => 'present',
+  version => '8',
+  java_se => 'jdk',
+}
   class {'scala':
   }
 }

2333dc009d294ee5e9731c4288f7beae2da3d9cf
baluevD <newarmlet@gmail.com>
2016-06-18 00:58:18 +0300

    Update puppet.pp

--- puppet.pp
+++ puppet.pp
@@ -1,9 +1,7 @@
 node default {
-  class { 'java8' :
-  }
-  class {'scala':
-  }
-  class { 'sbt':
+  class {'java8':  }
+  class {'scala':  }
+  class {'sbt':
   sbt_jar_version  => '0.13.11'
   sbt_java_opts    => '-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M'
   sbt_jar_path     => '/bin'

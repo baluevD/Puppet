71ae06958ea16fadd188d50e1f8a1ede1d03e719
baluevD <newarmlet@gmail.com>
2016-06-17 13:16:45 +0300

    adding sbt

--- puppet.pp
+++ puppet.pp
@@ -3,4 +3,9 @@ node default {
   }
   class {'scala':
   }
+  class { 'sbt':
+  sbt_jar_version  => '0.13.11'
+  sbt_java_opts    => '-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M'
+  sbt_jar_path     => '/bin'
+	}
 }

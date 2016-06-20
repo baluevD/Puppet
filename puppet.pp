2d3df0df0e8ff1c86fa8b68955d9685322cae3ae
baluevD <newarmlet@gmail.com>
2016-06-20 03:17:00 +0300

    Update puppet.pp

--- puppet.pp
+++ puppet.pp
@@ -1,7 +1,12 @@
 node default {
-  include java8
-  include 'scala'
-  class { 'sbt':
-  sbt_jar_version  => '0.13.8'
+#  include java8
+#  include 'scala'
+#  class { 'sbt':
+#  sbt_jar_version  => '0.13.8'
+#}
+include java7
+include 'scala'
+class { 'sbt':
+  sbt_jar_version  => '0.11.3'
 }
 }
